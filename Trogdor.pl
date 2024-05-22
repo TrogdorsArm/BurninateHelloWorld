8acfbd1d9cce8ce7799f2a7283e2c9ba89e55d5b
TrogdorsArm <38473835+TrogdorsArm@users.noreply.github.com>
2024-05-22 13:59:08 -0600

    Updates in forever

--- Trogdor.pl
+++ Trogdor.pl
@@ -2,10 +2,9 @@
 
 print "Hello World! BURNINATING ALL THE PEASANTS AND THEIR THATCH-ROOF COTTAGES!!!\n";
 
-open (FILE, "trogdor.asc") || die "Failed to open file: trogdor.asc : $^E\n";
+open (FILE, "trogdor.asc") or die "Failed to open file: trogdor.asc : $^E\n";
 while (<FILE>){
 	print;
-	sleep 1;
 }
 close(FILE);
 
